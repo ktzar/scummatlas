e1e1dc29f4bffdbbfbff78b9048e59920aa9ce3a
Miguel <miguelmail@gmail.com>
2015-02-27 17:17:17 +0000

    Add initial script parsing

--- src/scummatlas/room.go
+++ src/scummatlas/room.go
@@ -5,20 +5,20 @@ import (
 )
 
 type Image bool
-type Script bool
 type Box struct {
-    ulx int
-    uly int
-    urx int
-    ury int
-    lrx int
-    lry int
-    llx int
-    lly int
-    mask byte
-    flags byte
-    scale int
+	ulx   int
+	uly   int
+	urx   int
+	ury   int
+	lrx   int
+	lry   int
+	llx   int
+	lly   int
+	mask  byte
+	flags byte
+	scale int
 }
+type Script string
 type BoxMatrix bool
 
 type Room struct {
@@ -35,8 +35,8 @@ type Room struct {
 	ObjectScripts []Script
 	ExitScript    Script
 	EntryScript   Script
-	LocalScript   Script
-	Boxes       []Box
+	Boxes         []Box
+	LocalScripts  []Script
 	BoxMatrix     BoxMatrix
 }
 
@@ -60,7 +60,12 @@ func NewRoom(data []byte) *Room {
 			room.parseRMHD()
 		case "BOXD":
 			room.parseBOXD()
-		}
+		case "EXCD":
+			room.parseEXCD()
+		case "ENCD":
+			room.parseENCD()
+		case "LSCR":
+			room.parseLSCR()
 		case "RMIM":
 			room.parseRMIM()
 		}
@@ -73,14 +78,33 @@ func NewRoom(data []byte) *Room {
 	return room
 }
 
+func parseScriptBlock(data []byte) Script {
+	fmt.Println("Script size", BE32(data, 4))
+	return ""
+}
+
+func (r *Room) parseLSCR() {
+	script := parseScriptBlock(
+		r.data[r.offset : r.offset+r.getBlockSize()])
+	r.LocalScripts = append(r.LocalScripts, script)
+}
+
+func (r *Room) parseENCD() {
+	r.EntryScript = parseScriptBlock(r.data[r.offset : r.offset+r.getBlockSize()])
+}
+
+func (r *Room) parseEXCD() {
+	r.EntryScript = parseScriptBlock(r.data[r.offset : r.offset+r.getBlockSize()])
+}
+
 func (r *Room) parseBOXD() {
-    boxCount := LE16(r.data, r.offset + 8)
-    var boxOffset int
-    for i := 0 ; i < boxCount ; i ++ {
-        boxOffset = 10 + i * 20
-        box := NewBox(r.data[boxOffset:boxOffset + 20])
-	    r.Boxes = append(r.Boxes, box)
-    }
+	boxCount := LE16(r.data, r.offset+8)
+	var boxOffset int
+	for i := 0; i < boxCount; i++ {
+		boxOffset = 10 + i*20
+		box := NewBox(r.data[boxOffset : boxOffset+20])
+		r.Boxes = append(r.Boxes, box)
+	}
 }
 
 func (r *Room) parseRMHD() {
@@ -100,25 +124,28 @@ func (r Room) getBlockName() string {
 	return string(r.data[r.offset : r.offset+4])
 }
 
+func (r Room) getBlockSize() int {
+	return BE32(r.data, r.offset+4)
+}
+
 func (r *Room) nextBlock() {
-	blockSize := BE32(r.data, r.offset+4)
-	r.offset += blockSize
+	r.offset += r.getBlockSize()
 }
 
 func NewBox(data []byte) Box {
-    box := new(Box)
-    
-    box.ulx = LE16(data, 0)
-    box.uly = LE16(data, 2)
-    box.urx = LE16(data, 4)
-    box.ury = LE16(data, 6)
-    box.lrx = LE16(data, 8)
-    box.lry = LE16(data, 10)
-    box.llx = LE16(data, 12)
-    box.lly = LE16(data, 14)
-    box.mask = data[16]
-    box.flags = data[17]
-    box.scale = LE16(data, 18)
+	box := new(Box)
 
-    return *box
+	box.ulx = LE16(data, 0)
+	box.uly = LE16(data, 2)
+	box.urx = LE16(data, 4)
+	box.ury = LE16(data, 6)
+	box.lrx = LE16(data, 8)
+	box.lry = LE16(data, 10)
+	box.llx = LE16(data, 12)
+	box.lly = LE16(data, 14)
+	box.mask = data[16]
+	box.flags = data[17]
+	box.scale = LE16(data, 18)
+
+	return *box
 }
