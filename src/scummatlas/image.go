4a33f709af8200ee27479f97b7536ac2666a53c6
Miguel <miguelmail@gmail.com>
2015-03-16 17:37:26 +0000

    Add testing and fix substraction value

--- src/scummatlas/image.go
+++ src/scummatlas/image.go
@@ -169,38 +169,36 @@ func getCompressionMethod(stripNumber int, code byte) (
 	method = METHOD_UNKNOWN
 	transparent = NO_TRANSP
 	substraction = 0x00
+	if code == 0x01 {
+		method = METHOD_UNCOMPRESSED
+	} else if code >= 0x0e && code <= 0x30 {
+		method = METHOD_ONE
+	} else if code >= 0x40 && code <= 0x80 {
+		method = METHOD_TWO
+	}
 	switch {
 	case code == 0x01:
-		method = METHOD_UNCOMPRESSED
 	case 0x0e <= code && code <= 0x12:
 		direction = VERTICAL
-		method = METHOD_ONE
 		substraction = 0x0a
 	case 0x18 <= code && code <= 0x1c:
-		method = METHOD_ONE
 		substraction = 0x14
 	case 0x22 <= code && code <= 0x26:
-		method = METHOD_ONE
 		direction = VERTICAL
 		transparent = TRANSP
 		substraction = 0x1e
 	case 0x2c <= code && code <= 0x30:
-		method = METHOD_ONE
 		transparent = TRANSP
 		substraction = 0x28
 	case 0x40 <= code && code <= 0x44:
-		method = METHOD_TWO
 		substraction = 0x3c
 	case 0x54 <= code && code <= 0x58:
 		transparent = TRANSP
-		method = METHOD_TWO
 		substraction = 0x50
 	case 0x68 <= code && code <= 0x6c:
 		transparent = TRANSP
-		method = METHOD_TWO
 		substraction = 0x64
 	case 0x7c <= code && code <= 0x80:
-		method = METHOD_TWO
 		substraction = 0x78
 	}
 	out := fmt.Sprintf("%v\t0x%X\t", stripNumber, code)
