855adbb303aa8225f76ae170e4a4989691923677
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-03-24 13:52:11 +0000

    Add tests for box

---
+++ src/scummatlas/box_test.go
@@ -0,0 +1,32 @@
+package scummatlas
+
+import "testing"
+
+func TestBoxCorners(t *testing.T) {
+
+	aBox := Box{
+		ulx: 0,
+		uly: 10,
+		urx: 250,
+		ury: 20,
+		lrx: 20,
+		lry: 210,
+		llx: 250,
+		lly: 250,
+	}
+
+	corners := aBox.Corners()
+
+	var exp []Point;
+	exp = append(exp, Point{ 0,10})
+	exp = append(exp, Point{250,250})
+	exp = append(exp, Point{20, 210})
+	exp = append(exp, Point{250, 20})
+
+	for i := 0; i < 4 ; i ++ {
+		if corners[i] != exp[i] {
+			t.Errorf("Corner %v does not match", i)
+		}
+	}
+
+}
