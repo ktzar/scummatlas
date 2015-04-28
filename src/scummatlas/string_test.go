4ce4ea0d6f7f6a9caf6bed31aa4096b79a1d0a2e
miguelgonz <Miguel.Gonzalez@bbc.co.uk>
2015-04-28 17:50:21 +0100

    Add some tests for string

---
+++ src/scummatlas/string_test.go
@@ -0,0 +1,28 @@
+package scummatlas
+
+import "testing"
+
+func TestStringOpcode(t *testing.T) {
+	str, length := parsePrintOpcode([]byte{
+		0x0F, 0x41, 0x42, 0x43, 0x44, 0x00, 0xAA, 0xAA}, 0)
+	if str != "ABCD" {
+		t.Errorf("String %v is not right", str)
+	}
+	if length != 7 {
+		t.Errorf("Length %d is not right", length)
+	}
+
+	//OFFSET
+	str, length = parsePrintOpcode([]byte{
+		0xAA, 0xAA, 0x0F, 0x41, 0x42, 0x43, 0x44, 0x00, 0xAA, 0xAA}, 2)
+	if str != "ABCD" {
+		t.Errorf("String %v is not right", str)
+	}
+	if length != 7 {
+		t.Errorf("Length %d is not right", length)
+	}
+
+	//ENCODINGS
+	//TODO
+
+}
